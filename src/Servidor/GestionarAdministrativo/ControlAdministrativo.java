273cb2d029bbf8378561cc228fdb0595684baeae
payomaty666@gmail.com <payomaty666@gmail.com@46b9cd9e-a345-246f-1883-08143ed89cf9>
2013-09-07 04:59:48 +0000

    Agregados los controles del servidor

--- src/Servidor/GestionarAdministrativo/ControlAdministrativo.java
+++ src/Servidor/GestionarAdministrativo/ControlAdministrativo.java
@@ -4,7 +4,11 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Vector;
 
+import servidor.assembler.AdministrativoAssembler;
+import servidor.persistencia.AccesoBD;
+import servidor.persistencia.dominio.Administrativo;
 import common.DTOs.AdministrativoDTO;
+
 import common.GestionarAdministrativo.IControlAdministrativo;
 
 public class ControlAdministrativo extends UnicastRemoteObject implements IControlAdministrativo {
@@ -20,51 +24,74 @@ public class ControlAdministrativo extends UnicastRemoteObject implements IContr
 	}
 
 	@Override
-	public Long agregarAdministrativo(AdministrativoDTO administrativo)
-			throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public Long agregarAdministrativo(AdministrativoDTO administrativoDTO) throws Exception {
+		AccesoBD accesoBD = new AccesoBD();
+		Administrativo nuevoAdministrativo = AdministrativoAssembler.getAdministrativo(administrativoDTO);
+		Long id= new Long(0);
+		try{
+			accesoBD.iniciarTransaccion();
+			accesoBD.hacerPersistente(nuevoAdministrativo);
+			id=nuevoAdministrativo.getId();
+			accesoBD.concretarTransaccion();
+		} finally {
+			accesoBD.rollbackTransaccion();
+		}
+		return id;
 	}
 
 	@Override
 	public void eliminarAdministrativo(Long id) throws Exception {
-		// TODO Auto-generated method stub
-		
+		AccesoBD accesoBD = new AccesoBD();
+		try {
+			accesoBD.iniciarTransaccion();
+			Administrativo administrativo = AdministrativoAssembler.getAdministrativo(buscarAdministrativo(id));
+			accesoBD.eliminar(administrativo);
+			accesoBD.concretarTransaccion();
+		} finally {
+			accesoBD.rollbackTransaccion();
+		}
 	}
 
 	@Override
-	public void modificarAdministrativo(Long id, AdministrativoDTO modificado)
-			throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void modificarAdministrativo(Long id, AdministrativoDTO modificado) throws Exception {
+		AccesoBD accesoBD = new AccesoBD();
+		try{
+			accesoBD.iniciarTransaccion();
+			Administrativo administrativo = AdministrativoAssembler.getAdministrativo(buscarAdministrativo(id));
+
+			administrativo.setNombre_usuario(modificado.getNombre_usuario());
+			administrativo.setClave(modificado.getClave());
+			administrativo.setEmail(modificado.getEmail());
+			
+			accesoBD.concretarTransaccion();
+		} finally {
+			accesoBD.rollbackTransaccion();
+		}
 	}
 
 	@Override
 	public Vector<AdministrativoDTO> obtenerAdministrativo() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		AccesoBD accesoBD = new AccesoBD();
+		Vector administrativosDTO = new Vector();
+		try {
+			accesoBD.iniciarTransaccion();
+			Vector administrativos= (Vector) accesoBD.buscarPorFiltro(Administrativo.class,"nombre_usuario ascending");
+			for(int i=0; i<administrativos.size();i++){
+				AdministrativoDTO adminDTO = new AdministrativoDTO();
+				Administrativo admin = (Administrativo)administrativos.elementAt(i);
+				adminDTO.setId(admin.getId());
+				adminDTO.setNombre_usuario(admin.getNombre_usuario());
+				adminDTO.setClave(admin.getClave());
+				adminDTO.setEmail(admin.getEmail());
+				administrativosDTO.add(adminDTO);
+			}
+			accesoBD.concretarTransaccion();
+		}  finally{
+			accesoBD.rollbackTransaccion();
+		}
+		return administrativosDTO;
 	}
 
-	@Override
-	public Vector<AdministrativoDTO> obtenerAdministrativo(Long id)
-			throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Vector<AdministrativoDTO> obtenerAdministrativo_Nombre(
-			String nombre_administrativo) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Vector<AdministrativoDTO> obtenerAdministrativo_Email(String email)
-			throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public boolean existeAdministrativo(Long id) throws Exception {
